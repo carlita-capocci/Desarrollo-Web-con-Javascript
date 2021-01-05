4957d80d5a0414dd788fdd9b85213983dfa0c6e1
carlita-capocci <carla.a.capocci@gmail.com>
2021-01-04 21:03:45 -0300

    ej2

---
+++ ej2/js/recetario.js
@@ -0,0 +1,9 @@
+alert('A continuacion ingresa 3 ingredientes para una receta');
+
+var ingrediente1= prompt('Ingresa el primer ingrediente');
+var ingrediente2= prompt('Ingresa el segundo ingrediente');
+var ingrediente3= prompt('Ingresa el tercero ingrediente');
+
+var receta= 'Los ingredientes son ' + " " + ingrediente1 + " , " + ingrediente2 + " y " + ingrediente3;
+
+document.write(receta);
